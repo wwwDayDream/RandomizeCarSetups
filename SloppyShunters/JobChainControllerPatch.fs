ba9b9d4eaf78110454815b8da680b314715ff440
wwwDayDream <13383838+wwwDayDream@users.noreply.github.com>
2024-12-11 00:24:47 -0600

    Complete overhaul for B99+

---
+++ SloppyShunters/JobChainControllerPatch.fs
@@ -0,0 +1,88 @@
+﻿namespace SloppyShunters
+
+open DV.Logic.Job
+open DV.Simulation.Brake
+open DV.ThingTypes
+open HarmonyLib
+open UnityEngine
+
+[<HarmonyPatch(typeof<JobChainController>)>]
+module JobChainControllerPatch =
+    let private _random () = Random.Range(0f, 1f)
+    let private _dataLog (origin: string) (msg: string) =
+        Plugin.ModEntry.Value.Logger.Log $"[{origin}] {msg}"
+    let TryToMessWithLink (chance: float32) (train: TrainCar) (job: Job) =
+        if chance >= _random() then
+            let coupler = 
+                if train.frontCoupler.IsCoupled() then Some train.frontCoupler
+                elif train.rearCoupler.IsCoupled() then Some train.rearCoupler
+                else None
+            
+            if coupler.IsSome then
+                if _random() >= 0.4f then
+                    coupler.Value.DisconnectAirHose(false)
+                    _dataLog (job.ID + "|" + train.ID) "Disconnected Air Hose"
+                else
+                    coupler.Value.SetChainTight(false)
+                    if coupler.Value.coupledTo <> null then
+                        coupler.Value.coupledTo.SetChainTight(false)
+                    _dataLog (job.ID + "|" + train.ID) "Loosened Chain"
+            true
+        else
+            false
+    let TryToMessWithManualBrakes (chance: float32) (train: TrainCar) (job: Job) =
+        if train.brakeSystem.hasHandbrake && chance >= _random() then
+            _dataLog (job.ID + "|" + train.ID) "Adjusted Handbrake"
+            train.brakeSystem.SetHandbrakePosition (Random.Range (0.5f, 1f))
+            true
+        else
+            false
+    let TryToMessWithAirBrakes (chance: float32) (train: TrainCar) (job: Job) =
+        if chance > _random() then
+            let ran = _random()
+            let reverseCock (cock: HoseAndCock) =
+                cock.SetCock (not cock.cockOpen)
+                
+            let targets = [|
+                if ran > 0.66f then yield train.frontCoupler
+                elif ran > 0.33f then yield train.rearCoupler
+                else
+                    yield train.frontCoupler
+                    yield train.rearCoupler
+            |]
+            
+            for target in targets do
+                reverseCock target.hoseAndCock
+                if target.IsCoupled() then
+                    reverseCock target.coupledTo.hoseAndCock
+                _dataLog (job.ID + "|" + train.ID) "Swapped Cock(s)"
+            true
+        else
+            false
+    let Sloppify (trains: TrainCar array) (generatedJob: Job) =
+        CoroutineManager.Instance.StartCoroutine (seq {
+            yield WaitForSeconds (Coupler.AUTO_COUPLE_DELAY * 2f)
+            Plugin.ModEntry.Value.Logger.Log $"Sloppifying Job {generatedJob.ID} with {trains.Length} train(s).."
+            for train in trains do
+                let mutable messedWithLink = false
+                let mutable messedWithHandbrake = false
+                let mutable messedWithAirBrakes = false
+                for roll=1 to PluginConfig.RollsPerTrainCar do
+                    if not messedWithLink then
+                        messedWithLink <- TryToMessWithLink PluginConfig.ChanceToMessWithLink train generatedJob
+                    if not messedWithHandbrake then
+                        messedWithHandbrake <- TryToMessWithManualBrakes PluginConfig.ChanceToMessWithManualBrakes train generatedJob
+                    if not messedWithAirBrakes then
+                        messedWithAirBrakes <- TryToMessWithAirBrakes PluginConfig.ChanceToMessWithAirBrakes train generatedJob
+        } |> (fun s -> s.GetEnumerator())) |> ignore
+        ()
+    
+    [<HarmonyPatch("FinalizeSetupAndGenerateFirstJob")>] [<HarmonyPostfix>]
+    let JobGenerated (__instance: JobChainController) (jobLoadedFromSavegame: bool) =
+        if Plugin.Enabled then
+            if jobLoadedFromSavegame then
+                Plugin.ModEntry.Value.Logger.Log $"Skipping sloppification of Job[{__instance.currentJobInChain.ID}], Job is loaded from savegame!"
+            elif PluginConfig.JobsToRollOn |> List.contains __instance.currentJobInChain.jobType then
+                Sloppify (__instance.trainCarsForJobChain |> Array.ofSeq) __instance.currentJobInChain
+            else
+                Plugin.ModEntry.Value.Logger.Log $"Skipping sloppification of Job[{__instance.currentJobInChain.ID}], we don't roll on this job type."
---
+++ SloppyShunters/Library.fs
@@ -0,0 +1,121 @@
+﻿namespace SloppyShunters
+
+open System
+open System.IO
+open DV.ThingTypes
+open UnityEngine
+open UnityModManagerNet
+
+module PluginConfig =
+    let mutable ChanceToMessWithLink: float32 = 0.025f
+    let mutable ChanceToMessWithManualBrakes: float32 = 0.025f
+    let mutable ChanceToMessWithAirBrakes: float32 = 0.01f
+    let mutable RollsPerTrainCar: int = 1
+    let mutable JobsToRollOn: JobType list = [
+        JobType.Transport;JobType.ComplexTransport;JobType.EmptyHaul
+        JobType.ShuntingLoad;JobType.ShuntingUnload
+    ]
+    
+    let private configFilePath (modEntry: UnityModManager.ModEntry) =
+        Path.Combine(modEntry.Path, "config.dat")
+    
+    let Save (modEntry: UnityModManager.ModEntry) =
+        use writer = new BinaryWriter (File.OpenWrite(configFilePath modEntry))
+        writer.Write ChanceToMessWithLink
+        writer.Write ChanceToMessWithManualBrakes
+        writer.Write ChanceToMessWithAirBrakes
+        writer.Write RollsPerTrainCar
+        
+        writer.Write JobsToRollOn.Length
+        for job in JobsToRollOn do
+            writer.Write(byte job)
+        
+    let Load (modEntry: UnityModManager.ModEntry) =
+        if File.Exists (configFilePath modEntry) then
+            use reader = new BinaryReader (File.OpenRead(configFilePath (modEntry)))
+            try
+                ChanceToMessWithLink <- reader.ReadSingle()
+                ChanceToMessWithManualBrakes <- reader.ReadSingle()
+                ChanceToMessWithAirBrakes <- reader.ReadSingle()
+                RollsPerTrainCar <- reader.ReadInt32()
+               
+                let jobsCount = reader.ReadInt32()
+                let mutable jobs = []
+                for _=0 to jobsCount - 1 do
+                   let byteAsJob: JobType = Enum.ToObject(typeof<JobType>, reader.ReadByte()) :?> JobType
+                   jobs <- byteAsJob :: jobs
+                JobsToRollOn <- jobs
+            with _ ->
+                Save(modEntry)
+               
+module Plugin =
+    open HarmonyLib
+    open UnityModManagerNet
+    open Operators
+    
+    let mutable internal Patcher: Harmony option = None
+    let mutable internal Enabled: bool = false
+    let mutable internal ModEntry: UnityModManager.ModEntry option = None
+
+    let OnToggle (modEntry: UnityModManager.ModEntry) (value: bool): bool =
+        Enabled <- value
+        ModEntry <- Some modEntry
+        Patcher <- Harmony modEntry.Info.Id |> Some
+        
+        PluginConfig.Load modEntry
+        
+        if Patcher.IsNone then
+            Patcher <- Harmony("wwwDayDream.SloppyShunters") |> Some
+            
+        if Enabled then
+            Patcher.Value.PatchAll()
+        else
+            Patcher.Value.UnpatchAll()
+            
+        let methodCount = Patcher.Value.GetPatchedMethods() |> Seq.length
+        let methodCountPlural = if methodCount = 1 then "" else "s"
+        
+        ModEntry.Value.Logger.Log $"Patched {methodCount} method{methodCountPlural}."
+            
+        true
+    
+    let OnSaveGUI (modEntry: UnityModManager.ModEntry) =
+        PluginConfig.Save modEntry
+        
+    let OnGUI (modEntry: UnityModManager.ModEntry) =
+        GUILayout.Label "How many rolls of all chances per TrainCar"
+        let rollsPerTrainCarField, rollsPerTrainCarInt = Int32.TryParse(GUILayout.TextField(string PluginConfig.RollsPerTrainCar, 2))
+        PluginConfig.RollsPerTrainCar <- if rollsPerTrainCarField then rollsPerTrainCarInt else PluginConfig.RollsPerTrainCar
+        
+        GUILayout.Space(5f)
+        GUILayout.Label "What Job Types to roll on"
+        GUILayout.BeginHorizontal()
+        let mutable jobTypesToRandomize = []
+        let mutable idx = 1
+        for enumEntry in Enum.GetNames(typeof<JobType>) do
+            let curJobType = Enum.Parse(typeof<JobType>, enumEntry) :?> JobType
+            let curJobEnabled = GUILayout.Toggle (PluginConfig.JobsToRollOn |> List.contains curJobType, enumEntry)
+            if curJobEnabled then
+                jobTypesToRandomize <- curJobType :: jobTypesToRandomize
+            if idx % 3 = 0 then
+                GUILayout.EndHorizontal()
+                GUILayout.BeginHorizontal()
+            idx <- idx + 1
+        PluginConfig.JobsToRollOn <- jobTypesToRandomize
+        GUILayout.EndHorizontal()
+        
+        GUILayout.Space(5f)
+        GUILayout.Label "Chance to mess with link(s)"
+        PluginConfig.ChanceToMessWithLink <- GUILayout.HorizontalSlider(PluginConfig.ChanceToMessWithLink, 0f, 1f)
+        GUILayout.Label "Chance to mess with manual brakes" 
+        PluginConfig.ChanceToMessWithManualBrakes <- GUILayout.HorizontalSlider(PluginConfig.ChanceToMessWithManualBrakes, 0f, 1f)
+        GUILayout.Label "Chance to mess with air brakes" 
+        PluginConfig.ChanceToMessWithAirBrakes <- GUILayout.HorizontalSlider(PluginConfig.ChanceToMessWithAirBrakes, 0f, 1f)
+        
+        
+    let Load (modEntry: UnityModManager.ModEntry): bool =
+        modEntry.OnToggle <- OnToggle
+        modEntry.OnGUI <- OnGUI;
+        modEntry.OnSaveGUI <- OnSaveGUI;
+
+        true
